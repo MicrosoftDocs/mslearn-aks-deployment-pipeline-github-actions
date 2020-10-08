64f2de5eead9cb9016a6a5847284665a6e34e8fc
Lucas Santos <lhs.santoss@gmail.com>
2020-10-08 10:41:18 -0300

    Update init.sh

--- init.sh
+++ init.sh
@@ -29,7 +29,7 @@ az aks update \
     --resource-group $RESOURCE_GROUP_NAME \
     --attach-acr $ACR_NAME
 
-export DNS_NAME=`az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv`
+export DNS_NAME=`az aks show -g $RESOURCE_GROUP_NAME -n $AKS_NAME -o tsv --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o tsv`
 
 sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
 sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
