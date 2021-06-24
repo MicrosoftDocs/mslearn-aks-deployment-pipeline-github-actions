11e097cae2b41fac9fa3faf9aad2d4c78bd1f019
Lucas Santos <lhs.santoss@gmail.com>
2021-06-24 10:06:52 -0300

    Fix init script
    Signed-off-by: Lucas Santos <lhs.santoss@gmail.com>

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ echo "Defining variables..."
 export RESOURCE_GROUP_NAME=mslearn-gh-pipelines-$RANDOM
 export AKS_NAME=contoso-video
 export ACR_NAME=ContosoContainerRegistry$RANDOM
-declare DNS_GROUP_NAME=$RESOURCE_GROUP_NAME
 
 echo "Searching for resource group..."
 az group create -n $RESOURCE_GROUP_NAME -l eastus
@@ -27,21 +26,23 @@ echo "Creating ACR..."
 az acr create -n $ACR_NAME -g $RESOURCE_GROUP_NAME --sku basic
 az acr update -n $ACR_NAME --admin-enabled true
 
-echo "Attaching ACR to AKS..."
-export ACR_USERNAME=az acr credential show -n $ACR_NAME --query "username" -o tsv
-export ACR_PASSWORD=az acr credential show -n $ACR_NAME --query "passwords[0].value" -o tsv
+export ACR_USERNAME=$(az acr credential show -n $ACR_NAME --query "username" -o tsv)
+export ACR_PASSWORD=$(az acr credential show -n $ACR_NAME --query "passwords[0].value" -o tsv)
 
 az aks update \
     --name $AKS_NAME \
     --resource-group $RESOURCE_GROUP_NAME \
     --attach-acr $ACR_NAME
 
-export DNS_NAME=`az aks show -g $RESOURCE_GROUP_NAME -n $AKS_NAME -o tsv --query addonProfiles.httpApplicationRouting.config.HTTPApplicationRoutingZoneName -o tsv`
+export DNS_NAME=$(az network dns zone list -o json --query "[?contains(resourceGroup,'$RESOURCE_GROUP_NAME')].name" -o tsv)
+
+sed -i '' 's+!IMAGE!+'"$ACR_NAME"'/contoso-website+g' kubernetes/deployment.yaml
+sed -i '' 's+!DNS!+'"$DNS_NAME"'+g' kubernetes/ingress.yaml
 
 echo "Installation concluded, copy these values and store them, you'll use them later in this exercise:"
 echo "-> Resource Group Name: $RESOURCE_GROUP_NAME"
 echo "-> ACR Name: $ACR_NAME"
-echo "-> ACR Login Username: ACR_USERNAME"
+echo "-> ACR Login Username: $ACR_USERNAME"
 echo "-> ACR Password: $ACR_PASSWORD"
 echo "-> AKS Cluster Name: $ACR_NAME"
 echo "-> AKS DNS Zone Name: $DNS_NAME"
